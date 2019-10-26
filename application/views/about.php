aaa3c5a261eae6ef13a3a6edd41f597ae4ec2fd1
Shafira Herlinawati <shafiralina17@gmail.com>
2019-10-26 19:41:51 +0700

    login, about no fix yet

---
+++ application/views/about.php
@@ -0,0 +1,134 @@
+<div class="content">
+
+	<!-- about -->
+	<div class="about">
+		<div class="container">
+			<div class="row row-lg-eq-height">
+				
+				<!-- About Image -->
+				<div class="col-lg-6 order-lg-1 order-2">
+					<div class="about_image d-flex flex-column align-items-center justify-content-center">
+						<img src="<?php echo base_url('assets/images/servers.png')?>" alt="">
+					</div>
+				</div>
+
+				<!-- About Content -->
+				<div class="col-lg-6 order-lg-2 order-1">
+					<div class="about_content">
+						<div class="section_title"><h2>What we do @ BHost</h2></div>
+						<div class="about_text">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris velit arcu, scelerisque dignissim massa quis, mattis facilisis erat. Aliquam erat volutpat. Sed efficitur diam ut interdum ultricies. In a leo vel dolor tempor feugiat. Cras accumsan faucibus magna a imperdiet. Donec mi neque, pretium eu quam at, facilisis venenatis tortor. Suspendisse potenti.</p>
+						</div>
+						<div class="button about_button"><a href="#">read more</a></div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+
+	<!-- team -->
+	<div class="team">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="section_title_container text-center">
+						<div class="section_title"><h2>The Team</h2></div>
+						<div class="team_text">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris velit arcu, scelerisque dignissim massa quis, mattis facilisis erat. Aliquam erat volutpat. Sed efficitur diam ut interdum ultricies.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row team_row">
+				
+				<!-- Team Item -->
+				<div class="col-xl-3 col-md-6">
+					<div class="team_item">
+						<div class="team_image">
+							<img src="<?php echo base_url('assets/images/team_1.jpg')?>" alt="https://unsplash.com/@michaeldam">
+							<div class="team_overlay trans_400"></div>
+							<div class="team_social">
+								<ul class="d-flex flex-row align-items-center justify-content-between">
+									<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="team_content text-center">
+							<div class="team_name">Julia Williams</div>
+							<div class="team_title">Team Manager</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Team Item -->
+				<div class="col-xl-3 col-md-6">
+					<div class="team_item">
+						<div class="team_image">
+							<img src="<?php echo base_url('assets/images/team_2.jpg" alt="https://unsplash.com/@stairhopper')?>">
+							<div class="team_overlay trans_400"></div>
+							<div class="team_social">
+								<ul class="d-flex flex-row align-items-center justify-content-between">
+									<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="team_content text-center">
+							<div class="team_name">Michael Smith</div>
+							<div class="team_title">Manager</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Team Item -->
+				<div class="col-xl-3 col-md-6">
+					<div class="team_item">
+						<div class="team_image">
+							<img src="<?php echo base_url('assets/images/team_3.jpg')?>" alt="https://unsplash.com/@chrisjoelcampbell">
+							<div class="team_overlay trans_400"></div>
+							<div class="team_social">
+								<ul class="d-flex flex-row align-items-center justify-content-between">
+									<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="team_content text-center">
+							<div class="team_name">Jessica Brown</div>
+							<div class="team_title">Project Manager</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Team Item -->
+				<div class="col-xl-3 col-md-6">
+					<div class="team_item">
+						<div class="team_image">
+							<img src="<?php echo base_url('assets/images/team_4.jpg" alt="https://unsplash.com/@brookecagle')?>">
+							<div class="team_overlay trans_400"></div>
+							<div class="team_social">
+								<ul class="d-flex flex-row align-items-center justify-content-between">
+									<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="team_content text-center">
+							<div class="team_name">Jack Parker</div>
+							<div class="team_title">Developer</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+                                        
+</div>
